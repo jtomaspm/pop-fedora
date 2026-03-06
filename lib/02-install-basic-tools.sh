964904085295140473f62a236137f72cb4f28019
João Machado <joaotomachado@proton.me>
2026-03-06 20:14:47 +0000

    fix

--- lib/02-install-basic-tools.sh
+++ lib/02-install-basic-tools.sh
@@ -17,8 +17,8 @@ sudo fwupdmgr get-updates
 sudo fwupdmgr update
 set -e
 
-sudo dnf4 group install multimedia
-sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing # Switch to full FFMPEG.
-sudo dnf upgrade @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin # Installs gstreamer components. Required if you use Gnome Videos and other dependent applications.
+sudo dnf4 group install multimedia -y
+sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing  -y# Switch to full FFMPEG.
+sudo dnf upgrade @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y # Installs gstreamer components. Required if you use Gnome Videos and other dependent applications.
 sudo dnf group install -y sound-and-video # Installs useful Sound and Video complementary packages.
 
