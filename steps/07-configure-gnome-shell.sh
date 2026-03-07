f8226fd6a0a64e2e4ed68a2a3c36ebef24865a19
João Machado <joaotomachado@proton.me>
2026-03-07 19:23:25 +0000

    add keybinds

--- steps/07-configure-gnome-shell.sh
+++ steps/07-configure-gnome-shell.sh
@@ -71,13 +71,46 @@ run_user gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac
 run_user gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
 
 pf_log_info "Setting keyboard shortcuts"
+
 run_user gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
     "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-file-manager/']"
-run_user gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-file-manager/ name "File Manager"
+run_user gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-file-manager/ name "FileManager"
 run_user gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-file-manager/ command "nautilus --new-window"
 run_user gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-file-manager/ binding "<Super>e"
+
 run_user gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
     "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-terminal/']"
 run_user gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-terminal/ name "Terminal"
 run_user gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-terminal/ command "ghostty"
 run_user gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom-terminal/ binding "<Super>Return"
+
+run_user gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q', '<Alt>F4']"
+run_user gsettings set org.gnome.settings-daemon.plugins.media-keys www "['<Super>b']"
+
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>1']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Super><Shift>2']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Super><Shift>3']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Super><Shift>4']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<Super><Shift>5']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<Super><Shift>6']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<Super><Shift>7']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Super><Shift>8']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Super><Shift>9']"
+run_user gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
+
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
+
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Super>Page_Up', '<Super>KP_Prior', '<Super><Alt>Left', '<Control><Alt>Left', '<Super>a']"
+run_user gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Super>Page_Down', '<Super>KP_Next', '<Super><Alt>Right', '<Control><Alt>Right', '<Super>s']"
+
+run_user gsettings set org.gnome.shell.keybindings toggle-overview "['<Super>w']"
+
