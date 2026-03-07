f75c993cb988a37bd76dfe8d1463608c71f2aedc
pop <joaotomachado@proton.me>
2026-03-07 11:32:36 +0000

    update

--- lib/05-install-software.sh
+++ lib/05-install-software.sh
@@ -24,6 +24,7 @@ flatpak install --system -y flathub com.stremio.Stremio
 flatpak install --system -y flathub com.mattjakeman.ExtensionManager
 flatpak install --system -y flathub com.vysp3r.ProtonPlus
 flatpak install --system -y flathub org.onlyoffice.desktopeditors
+flatpak install --system -y flathub io.ente.auth
 
 dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo -y
 dnf install gh --repo gh-cli -y
